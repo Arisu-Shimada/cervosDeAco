00d7c1d21c05cdc8c51a117baf5744c0902adb88
Tiago Rodovalho Gonçalves <trodovalhogoncalves@gmail.com>
2026-08-31 00:25:26 -0400

    integrando codigo em python

--- ev3/seguidor.py
+++ ev3/seguidor.py
@@ -6,7 +6,8 @@ from ev3dev2.sensor.lego import InfraredSensor
 #from ev3dev2.sensor import INPUT_1
 #from ev3dev2.sensor import INPUT_2
 from ev3dev.ev3 import *
-from time import sleep
+from ev3dev2.led import Leds
+from time import sleep, time
 
 # Define os motores
 tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
@@ -133,6 +134,53 @@ def desviarBloco():
                 tank_drive.on(-50, 50)
             veri = True
     veri = False
+def verde():
+    leds = Leds()
+    leds.all_off()
+    tank_drive.on_for_rotations(50, 50, 0.02)
+    tank_drive.off()
+    time.sleep(0.5)
+    # Le os valores dos sensores
+    valor_esq = sensor_esq.value()
+    valor_dir = sensor_dir.value()
+    valor_dist = sensor_dist.value()
+    if (sensor_esq == 3 and sensor_dir == 3):
+        leds.set_color("LEFT", "RED")
+        leds.set_color("RIGHT", "RED")
+        tank_drive.on_for_rotations(-50, 50, 0.9)
+        while(sensor_esq != 1):
+            # Le os valores dos sensores
+            valor_esq = sensor_esq.value()
+            valor_dir = sensor_dir.value()
+            valor_dist = sensor_dist.value()
+            tank_drive.on(-50, 50)
+            time.sleep(0.01)
+    if (sensor_esq == 3 and sensor_dir != 3):
+        leds.set_color("LEFT", "ORANGE")
+        leds.set_color("RIGHT", "ORANGE")
+        tank_drive.on_for_rotations(-50, 50, 0.5)
+        while(sensor_esq != 1):
+            # Le os valores dos sensores
+            valor_esq = sensor_esq.value()
+            valor_dir = sensor_dir.value()
+            valor_dist = sensor_dist.value()
+            tank_drive.on(-50, 50)
+            time.sleep(0.01)
+        tank_drive.on_for_rotations(50, -50, 0.1)
+    if (sensor_esq != 3 and sensor_dir == 3):
+        leds.set_color("LEFT", "GREEN")
+        leds.set_color("RIGHT", "GREEN")
+        tank_drive.on_for_rotations(50, -50, 0.5)
+        while(sensor_esq != 1):
+            # Le os valores dos sensores
+            valor_esq = sensor_esq.value()
+            valor_dir = sensor_dir.value()
+            valor_dist = sensor_dist.value()
+            tank_drive.on(50, -50)
+            time.sleep(0.01)
+        tank_drive.on_for_rotations(-50, 50, 0.1)
+    leds.set_color("LEFT", "GREEN")
+    leds.set_color("RIGHT", "GREEN")
 
 
 
@@ -140,7 +188,28 @@ def desviarBloco():
 if __name__ == "__main__":
     # Inicia o loop para seguir a linha
     try:
-        seguir_linha()
+        while True:
+            while(not Button().enter):
+                print("Aguardando para iniciar o seguimento da linha...")
+                sleep(0.5)  # Pequeno delay para evitar leitura rápida do botão
+            while(not Button().enter):
+                # Le os valores dos sensores
+                valor_esq = sensor_esq.value()
+                valor_dir = sensor_dir.value()
+                valor_dist = sensor_dist.value()
+                if (valor_esq != 1 and valor_esq != 3 and valor_dir != 1 and valor_dir != 3):
+                    # Ambos sensores sobre branco (fora da linha), segue em frente
+                    tank_drive.on(velocidade, velocidade)    
+                if (valor_esq != 6 and valor_esq != 0 or sensor_dir != 6 and valor_dir != 0):
+                    # Sensor esquerdo sobre a linha (preto), curva para a esquerda
+                    seguir_linha()
+                    if (sensor_dir == 3 or sensor_esq == 3):
+                        verde()
+                if (sensor_dist < 4):
+                    desviarBloco()
+                if (sensor_esq == 5 and sensor_dir == 5):
+                    tank_drive.off()
+                    break
     except KeyboardInterrupt:
         tank_drive.off()
         print("Programa interrompido pelo usuario.")
